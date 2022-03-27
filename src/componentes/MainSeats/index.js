ed11e470088e83a8cd97e5860789de3a1df0d628
Nicole J. C. Lemes <nicolejdecarvalho@gmail.com>
2022-03-26 22:19:46 -0300

    show unavailable seats

--- src/componentes/MainSeats/index.js
+++ src/componentes/MainSeats/index.js
@@ -1,50 +1,59 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './style.css'
 
 export default function SeatsPage(){
-    return(
+
+    const {sessionID} = useParams();
+    const [seats, setSeats] = useState(null)
+    //const [selected, setSelected] = useState('seat available')
+    let newClass
+    
+    useEffect(() => {
+        const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${sessionID}/seats`);
+        promise.then((response) => setSeats(response.data));
+    },[])
+
+    return seats === null ? (
+        <p className='loading'>loading...</p>
+    ) : (
         <main className='main-seats'>
             <h2>Selecione o(s) assento(s)</h2>
             <section>
                 <div className='seats'>
-                    <div className='seat'>01</div>
-                    <div className='seat'>02</div>
-                    <div className='seat'>03</div>
-                    <div className='seat'>04</div>
-                    <div className='seat'>01</div>
-                    <div className='seat'>02</div>
-                    <div className='seat'>03</div>
-                    <div className='seat'>04</div>
-                    <div className='seat'>01</div>
-                    <div className='seat'>02</div>
-                    <div className='seat'>03</div>
-                    <div className='seat'>04</div>
+                    {(seats.seats).map((seat) => {
+                        seat.isAvailable === false ? newClass = 'seat unavailable' : newClass = 'seat available'
+                        //if(seat.isAvailable === false){setSelected('seat unavailable')}
+                        //return <div onClick={() => {if(seat.isAvailable === false){alert('indisponivel')} else {setSelected('seat selected')}}} key={seat.id} className={selected}>{seat.name}</div>
+                        return <div onClick={() => {if(seat.isAvailable === false){alert('indisponivel')} else {newClass = 'seat selected'}}} key={seat.id} className={newClass}>{seat.name}</div>
+                    })}
                 </div>
                 <div className='subtitle'>
-                    <div className='selected'>
-                        <div className='seat'></div>
+                    <div>
+                        <div className='seat selected'></div>
                         <p>Selecionado</p>
                     </div>
-                    <div className='available'>
-                        <div className='seat'></div>
+                    <div>
+                        <div className='seat available'></div>
                         <p>Disponível</p>
                     </div>
-                    <div className='unavailable'>
-                        <div className='seat'></div>
+                    <div>
+                        <div className='seat unavailable'></div>
                         <p>Indisponível</p>
                     </div>
                 </div>
                 <p>Nome do comprador:</p>
-                <input placeholder='Digite seu nome...'></input>
+                <input type="text" placeholder='Digite seu nome...'></input>
                 <p>CPF do comprador:</p>
-                <input placeholder='Digite seu CPF...'></input>
+                <input type="text" placeholder='Digite seu CPF...'></input>
                 <Link to="/sucesso"><button>Reservar assento(s)</button></Link>
             </section>
             <footer>
-                <img src="https://image.tmdb.org/t/p/w220_and_h330_face/zzXFM4FKDG7l1ufrAkwQYv2xvnh.jpg" alt="movie" />
+                <img src={seats.movie.posterURL} alt="movie" />
                 <div>
-                    <p>Nome do filme</p>
-                    <p>Dados da sessão</p>
+                    <p>{seats.movie.title}</p>
+                    <p>{seats.day.weekday} - {seats.name}</p>
                 </div>
             </footer>
         </main>
